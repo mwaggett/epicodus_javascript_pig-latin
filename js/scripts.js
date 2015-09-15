f0c1d85da6cd800bcc90da731f86d46e1622d17e
Molly Waggett & Andrea Padgett <student@epicodus.com>
2015-09-15 10:20:00 -0700

    Write pigLatin function with subfunctions for returning the first letter of a word, removing the first letter, checking for vowels, 'qu', and 'y', and adding 'ay' to the end; specs are met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,57 @@
-// var functionName = function(params) {
-//
-// };
+var pigLatin = function(word) {
+  if (checkIfY(returnFirstLetter(word))) {
+    var letter = returnFirstLetter(word);
+    word = appendLetterToEnd(removeFirstLetter(word), letter);
+  }
+  while (!checkIfVowel(returnFirstLetter(word))) {
+    if (checkIfQU(word)) {
+      word = word.slice(2) + "qu";
+    } else {
+      var letter = returnFirstLetter(word);
+      word = appendLetterToEnd(removeFirstLetter(word), letter);
+    }
+  }
+  return addAy(word);
+}
+
+var checkIfVowel = function(letter) {
+  letter = letter.toLowerCase();
+  if (letter === "a" || letter === "e" || letter === "i"
+        || letter === "o" || letter === "u" || letter === "y") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var checkIfY = function(letter) {
+  return letter === "y";
+}
+
+var checkIfQU = function(word) {
+  var firstTwo = word.slice(0,2);
+  if (firstTwo === "qu") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var returnFirstLetter = function(word) {
+  return word.slice(0,1);
+}
+
+var removeFirstLetter = function(word) {
+  return word.slice(1);
+}
+
+var appendLetterToEnd = function(word, letter) {
+  return word + letter;
+};
+
+var addAy = function(word) {
+  return word + "ay";
+}
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,72 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('pigLatin', function(){
+  it("attaches consonents and ay to end of word", function() {
+    expect(pigLatin("group")).to.equal("oupgray");
+  });
+
+  it("just adds 'ay' to end when word starts with vowel", function() {
+    expect(pigLatin("apple")).to.equal("appleay");
+  });
+
+  it("moves 'u' to end of word along with 'q'", function() {
+    expect(pigLatin("squeal")).to.equal("ealsquay");
+  });
+
+  it("treats 'y' as a vowel in middle of word", function() {
+    expect(pigLatin("rhythm")).to.equal("ythmrhay");
+  });
+});
+
+
+describe('checkIfVowel', function() {
+  it("returns true when given a vowel", function() {
+    expect(checkIfVowel("e")).to.equal(true);
+  });
+
+  it("returns false when given a consonant", function() {
+    expect(checkIfVowel("p")).to.equal(false);
+  });
+});
+
+describe('checkIfY', function() {
+  it("returns true when 'y'", function() {
+    expect(checkIfY("y")).to.equal(true);
+  });
+
+  it("returns false when not 'y'", function() {
+    expect(checkIfY("p")).to.equal(false);
+  });
+});
+
+describe('checkIfQU', function() {
+  it("returns true when starts with 'qu'", function() {
+    expect(checkIfQU("quiet")).to.equal(true);
+  });
+
+  it("returns false when doesn't start with 'qu'", function() {
+    expect(checkIfQU("pickle")).to.equal(false);
+  });
+});
+
+describe('returnFirstLetter', function() {
+  it("returns first letter of a word", function() {
+    expect(returnFirstLetter("cat")).to.equal("c");
+  });
+});
+
+describe('removeFirstLetter', function() {
+  it("removes first letter of a word", function() {
+    expect(removeFirstLetter("cat")).to.equal("at");
+  });
+});
+
+describe('appendLetterToEnd', function() {
+  it("appends given letter to end of given word", function() {
+    expect(appendLetterToEnd("at", "c")).to.equal("atc");
+  });
+});
+
+describe('addAy', function() {
+  it("adds 'ay' to the end of a string", function() {
+    expect(addAy("atc")).to.equal("atcay");
+  });
+});
